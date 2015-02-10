0fad848205b7b90a5f8113a30aea5a9652f249f3
Nick Slager <nick@13secondstoburn.com>
2015-02-10 16:13:05 -0500

    Initial Sudoku Helper web help import

---
+++ SudokuHelperWeb/SudokuHelperWeb/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SudokuHelperWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "";
+
+            return View();
+        }
+
+        public ActionResult Rules()
+        {
+            ViewBag.Title = "Rules of Sudoku";
+            return View();
+        }
+
+        public ActionResult AppControls()
+        {
+            ViewBag.Title = "How to Use the App";
+            return View();
+        }
+
+        public ActionResult Techniques()
+        {
+            ViewBag.Title = "Solving Techniques";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
