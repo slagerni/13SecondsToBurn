bd516bab30cd1cb57457e8264e1800ab45cc9dc8
Nick Slager <nick@13secondstoburn.com>
2015-09-10 16:28:10 -0400

    minor modifications for title/meta tags

--- SudokuHelperWeb/SudokuHelperWeb/Controllers/HomeController.cs
+++ SudokuHelperWeb/SudokuHelperWeb/Controllers/HomeController.cs
@@ -10,45 +10,50 @@ namespace SudokuHelperWeb.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "";
-
+            ViewBag.Title = "Sudoku Helper - Android App and Solving Techniques";
+            ViewBag.HeaderTitle = "";
             return View();
         }
 
         public ActionResult Rules()
         {
-            ViewBag.Title = "Rules of Sudoku";
+            ViewBag.Title = "Sudoku Helper - Rules of Sudoku";
+            ViewBag.HeaderTitle = "Rules of Sudoku";
             return View();
         }
 
         public ActionResult AppControls()
         {
-            ViewBag.Title = "How to Use the App";
+            ViewBag.Title = "Sudoku Helper - How to Use the App";
+            ViewBag.HeaderTitle = "How to Use the App";
             return View();
         }
 
         public ActionResult Techniques()
         {
-            ViewBag.Title = "Solving Techniques";
+            ViewBag.Title = "Sudoku Helper - Solving Techniques";
+            ViewBag.HeaderTitle = "Solving Techniques";
             return View();
         }
 
         public ActionResult GetIt()
         {
-            ViewBag.Title = "Get Sudoku Helper";
+            ViewBag.Title = "Sudoku Helper - Get It";
+            ViewBag.HeaderTitle = "Get It!";
             return View();
         }
 
         public ActionResult History()
         {
-            ViewBag.Title = "Sudoku Helper History";
+            ViewBag.Title = "Sudoku Helper - History";
+            ViewBag.HeaderTitle = "History";
             return View();
         }
 
         public ActionResult Contact()
         {
-            //ViewBag.Message = "Your contact page.";
-
+            ViewBag.Message = "Sudoku Helper - Contact";
+            ViewBag.Title = "Sudoku Helper - Contact";
             return View();
         }
     }
