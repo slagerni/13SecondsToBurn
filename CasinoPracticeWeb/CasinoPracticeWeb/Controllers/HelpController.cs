da55f43b6e8790a68c5c43abbe40e6e9bd835a5f
Nick Slager <nick@13secondstoburn.com>
2016-07-28 15:25:13 -0400

    add the casino web app just to have a backup

---
+++ CasinoPracticeWeb/CasinoPracticeWeb/Controllers/HelpController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasinoPracticeWeb.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Casino Practice - Everyone needs a little Help sometimes";
+            return View();
+        }
+
+        public ActionResult Blackjack()
+        {
+            ViewBag.Title = "Casino Practice - Blackjack";
+            return View();
+        }
+
+        public ActionResult Crazy4Poker()
+        {
+            ViewBag.Title = "Casino Practice - Crazy 4 Poker";
+            return View();
+        }
+
+        public ActionResult Craps()
+        {
+            ViewBag.Title = "Casino Practice - Craps";
+            return View();
+        }
+
+        public ActionResult CaribbeanStud()
+        {
+            ViewBag.Title = "Casino Practice - Caribbean Stud Poker";
+            return View();
+        }
+
+        public ActionResult LetItRide()
+        {
+            ViewBag.Title = "Casino Practice - Let It Ride";
+            return View();
+        }
+
+        public ActionResult Roulette()
+        {
+            ViewBag.Title = "Casino Practice - Roulette";
+            return View();
+        }
+
+        public ActionResult ThreeCardPoker()
+        {
+            ViewBag.Title = "Casino Practice - ThreeCardPoker";
+            return View();
+        }
+
+    }
+}
---
+++ CasinoPracticeWeb/CasinoPracticeWeb/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasinoPracticeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult GetIt()
+        {
+            ViewBag.Title = "Casino Practice - Get It";
+            ViewBag.HeaderTitle = "Get It!";
+            return View();
+        }
+    }
+}
