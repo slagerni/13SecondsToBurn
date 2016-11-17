b6b6631233fdff813707d06f9625242d1983deee
Nick Slager <nick@13secondstoburn.com>
2016-11-17 12:38:41 -0500

    Make betting in Roulette a little easier by splitting out the bet types and adding buttons to toggle what type of bet (center, line, corner)

--- CasinoPractice/core/src/com/thirteensecondstoburn/CasinoPractice/Screens/RouletteScreen.java
+++ CasinoPractice/core/src/com/thirteensecondstoburn/CasinoPractice/Screens/RouletteScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.thirteensecondstoburn.CasinoPractice.Actors.ActionCompletedListener;
@@ -30,19 +31,30 @@ public class RouletteScreen extends TableScreen implements ActionCompletedListen
 
     public RouletteScreen(CasinoPracticeGame game) {
         super(game);
+        menuButtonColor = game.getAssets().getSkin().getColor("menuButtonColor");
     }
+
+    private enum BetType{ ON, LINE, CORNER };
 
     public static final StatisticType Spun = new StatisticType("spun", "Spun");
     Image tableImage;
     TableButton spinButton;
     MultiLineTableButton undoBetButton;
-    List<BettingSpot> bettingSpots;
+    List<BettingSpot> onSpots;
+    List<BettingSpot> lineSpots;
+    List<BettingSpot> cornerSpots;
     RouletteWheel wheel;
     RouletteNumberBoard numberBoard;
     public static boolean isEuropean = true;
     LinkedList<BetHistory> betHistory = new LinkedList<>();
+    BetType currentBettingType = BetType.CORNER;
+    Button centerBetButton;
+    Button lineBetButton;
+    Button cornerBetButton;
 
-    @Override
+    Color menuButtonColor;
+    Color notSelectedColor = Color.GRAY;
+    
     public void actionCompleted(Actor caller) {
         numberBoard.push(wheel.getNumber());
         calculateWinnings(wheel.getNumber());
@@ -89,84 +101,93 @@ public class RouletteScreen extends TableScreen implements ActionCompletedListen
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 if (undoBetButton.isInside(x, y)) {
-                    if(betHistory.size() == 0) return;
+                    if (betHistory.size() == 0) return;
 
                     BetHistory last = betHistory.pop();
                     int currentWager = 0;
-                    for (BettingSpot spot : bettingSpots) {
+                    for (BettingSpot spot : onSpots) {
+                        currentWager += spot.stack.getTotal();
+                    }
+                    for (BettingSpot spot : lineSpots) {
+                        currentWager += spot.stack.getTotal();
+                    }
+                    for (BettingSpot spot : cornerSpots) {
                         currentWager += spot.stack.getTotal();
                     }
                     currentWager -= last.amount;
 
                     placeBet(last.stack, -last.amount);
                     leftSide.setWagerText("" + (currentWager + currentWager));
-                    if(betHistory.size() == 0) {
+                    if (betHistory.size() == 0) {
                         undoBetButton.setVisible(false);
                     }
                 }
             }
         });
 
-        bettingSpots = new ArrayList<>();
-        bettingSpots.add(new BettingSpot(new GridPoint2(662, 703), 1, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18})); // 1 - 18
-        bettingSpots.add(new BettingSpot(new GridPoint2(1232, 703), 1, new int[]{19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36})); // 19 - 36
+        onSpots = new ArrayList<>();
+        lineSpots = new ArrayList<>();
+        cornerSpots = new ArrayList<>();
+
+        onSpots.add(new BettingSpot(new GridPoint2(662, 703), 1, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18})); // 1 - 18
+        onSpots.add(new BettingSpot(new GridPoint2(1232, 703), 1, new int[]{19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36})); // 19 - 36
 
         // loop to add the single numbers
         for(int i = 0; i < 12; i++) {
-            bettingSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 588), 35, new int[]{i*3 + 3})); // top line
-            bettingSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 460), 35, new int[]{i*3 + 2})); // middle line
-            bettingSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 342), 35, new int[]{i*3 + 1})); // bottom line
+            onSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 588), 35, new int[]{i*3 + 3})); // top line
+            onSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 460), 35, new int[]{i*3 + 2})); // middle line
+            onSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 342), 35, new int[]{i*3 + 1})); // bottom line
         }
         if(isEuropean) {
-            bettingSpots.add(new BettingSpot(new GridPoint2(336, 460), 35, new int[]{0})); // single 0
+            onSpots.add(new BettingSpot(new GridPoint2(336, 460), 35, new int[]{0})); // single 0
         } else {
             // American 00 = 37
-            bettingSpots.add(new BettingSpot(new GridPoint2(336, 524), 35, new int[]{37})); // single 00
-            bettingSpots.add(new BettingSpot(new GridPoint2(336, 404), 35, new int[]{0})); // single 0
+            onSpots.add(new BettingSpot(new GridPoint2(336, 524), 35, new int[]{37})); // single 00
+            onSpots.add(new BettingSpot(new GridPoint2(336, 404), 35, new int[]{0})); // single 0
         }
 
-        bettingSpots.add(new BettingSpot(new GridPoint2(1574, 588), 2, new int[]{3,6,9,12,15,18,21,24,27,30,33,36})); // top dozen
-        bettingSpots.add(new BettingSpot(new GridPoint2(1574, 460), 2, new int[]{2,5,8,11,14,17,20,23,26,29,32,35})); // middle dozen
-        bettingSpots.add(new BettingSpot(new GridPoint2(1574, 342), 2, new int[]{1,4,7,10,13,16,19,22,25,28,31,34})); // bottom dozen
+        onSpots.add(new BettingSpot(new GridPoint2(1574, 588), 2, new int[]{3,6,9,12,15,18,21,24,27,30,33,36})); // top dozen
+        onSpots.add(new BettingSpot(new GridPoint2(1574, 460), 2, new int[]{2,5,8,11,14,17,20,23,26,29,32,35})); // middle dozen
+        onSpots.add(new BettingSpot(new GridPoint2(1574, 342), 2, new int[]{1,4,7,10,13,16,19,22,25,28,31,34})); // bottom dozen
 
         // loop to add the left/right split numbers
         for(int i = 0; i < 11; i++) {
-            bettingSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 588), 17, new int[]{i*3 + 3, (i+1)*3 + 3})); // top line
-            bettingSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 460), 17, new int[]{i*3 + 2, (i+1)*3 + 3})); // middle line
-            bettingSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 342), 17, new int[]{i*3 + 1, (i+1)*3 + 3})); // bottom line
+            lineSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 588), 17, new int[]{i*3 + 3, (i+1)*3 + 3})); // top line
+            lineSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 460), 17, new int[]{i*3 + 2, (i+1)*3 + 3})); // middle line
+            lineSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 342), 17, new int[]{i*3 + 1, (i+1)*3 + 3})); // bottom line
         }
 
         // more split numbers, this time the top/bottom split
         for(int i = 0; i < 12; i++) {
-            bettingSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 530), 17, new int[]{i * 3 + 2, i * 3 + 3})); // top line
-            bettingSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 410), 17, new int[]{i * 3 + 1, i * 3 + 2})); // bottom line
+            lineSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 530), 17, new int[]{i * 3 + 2, i * 3 + 3})); // top line
+            lineSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 410), 17, new int[]{i * 3 + 1, i * 3 + 2})); // bottom line
         }
 
         // loop to add the corner bets
         for(int i=0; i < 11; i++) {
-            bettingSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 530), 8, new int[]{i*3 + 2, (i+1)*3 + 2, i*3 + 3, (i+1)*3 + 3})); // top line
-            bettingSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 410), 8, new int[]{i*3 + 1, (i+1)*3 + 1, i*3 + 2, (i+1)*3 + 2})); // bottom line
+            cornerSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 530), 8, new int[]{i*3 + 2, (i+1)*3 + 2, i*3 + 3, (i+1)*3 + 3})); // top line
+            cornerSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 410), 8, new int[]{i*3 + 1, (i+1)*3 + 1, i*3 + 2, (i+1)*3 + 2})); // bottom line
         }
 
         // loop to add the street bets
         for(int i=0; i < 12; i++) {
-            bettingSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 650), 11, new int[]{i * 3 +1, i * 3 + 2, i * 3 + 3}));
+            lineSpots.add(new BettingSpot(new GridPoint2(428 + i * 95, 650), 11, new int[]{i * 3 +1, i * 3 + 2, i * 3 + 3}));
         }
 
         // loop to add the line bets
         for (int i=0; i < 11; i++) {
-            bettingSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 292), 5, new int[]{i * 3 +1, i * 3 + 2, i * 3 + 3, (i+1) * 3 +1, (i+1) * 3 + 2, (i+1) * 3 + 3}));
+            onSpots.add(new BettingSpot(new GridPoint2(475 + i * 95, 292), 5, new int[]{i * 3 +1, i * 3 + 2, i * 3 + 3, (i+1) * 3 +1, (i+1) * 3 + 2, (i+1) * 3 + 3}));
         }
 
-        bettingSpots.add(new BettingSpot(new GridPoint2(380, 292), 6, new int[]{37,0,1,2,3})); // basket 00 = 37
+        onSpots.add(new BettingSpot(new GridPoint2(380, 292), 6, new int[]{37,0,1,2,3})); // basket 00 = 37
 
-        bettingSpots.add(new BettingSpot(new GridPoint2(568, 228), 2, new int[]{1,2,3,4,5,6,7,8,9,10,11,12})); // 1 - 12
-        bettingSpots.add(new BettingSpot(new GridPoint2(952, 228), 2, new int[]{13,14,15,16,17,18,19,20,21,22,23,24})); // 13 - 24
-        bettingSpots.add(new BettingSpot(new GridPoint2(1328, 228), 2, new int[]{25,26,27,28,29,30,31,32,33,34,35,36})); // 25 - 36
-        bettingSpots.add(new BettingSpot(new GridPoint2(540, 110), 1, new int[]{2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36})); // even
-        bettingSpots.add(new BettingSpot(new GridPoint2(812, 110), 1, new int[]{1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36})); // red
-        bettingSpots.add(new BettingSpot(new GridPoint2(1094, 110), 1, new int[]{2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35})); // black
-        bettingSpots.add(new BettingSpot(new GridPoint2(1390, 110), 1, new int[]{1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35})); //odd
+        onSpots.add(new BettingSpot(new GridPoint2(568, 228), 2, new int[]{1,2,3,4,5,6,7,8,9,10,11,12})); // 1 - 12
+        onSpots.add(new BettingSpot(new GridPoint2(952, 228), 2, new int[]{13,14,15,16,17,18,19,20,21,22,23,24})); // 13 - 24
+        onSpots.add(new BettingSpot(new GridPoint2(1328, 228), 2, new int[]{25,26,27,28,29,30,31,32,33,34,35,36})); // 25 - 36
+        onSpots.add(new BettingSpot(new GridPoint2(540, 110), 1, new int[]{2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36})); // even
+        onSpots.add(new BettingSpot(new GridPoint2(812, 110), 1, new int[]{1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36})); // red
+        onSpots.add(new BettingSpot(new GridPoint2(1094, 110), 1, new int[]{2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35})); // black
+        onSpots.add(new BettingSpot(new GridPoint2(1390, 110), 1, new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35})); //odd
 
         stage.addListener(new ActorGestureListener() {
             @Override
@@ -174,15 +195,31 @@ public class RouletteScreen extends TableScreen implements ActionCompletedListen
                 BettingSpot closest = null;
                 float minDistance = 1000000000;
                 int currentWager = 0;
-                for (BettingSpot spot : bettingSpots) {
+                for (BettingSpot spot : onSpots) {
                     currentWager += spot.stack.getTotal();
                     float distance = spot.distance(x, y);
-                    if(distance < minDistance) {
+                    if (distance < minDistance && currentBettingType == BetType.ON) {
                         closest = spot;
                         minDistance = distance;
                     }
                 }
-                if(minDistance > 40.0f) return; // has to be somewhat close to the point we're looking for
+                for (BettingSpot spot : lineSpots) {
+                    currentWager += spot.stack.getTotal();
+                    float distance = spot.distance(x, y);
+                    if (distance < minDistance && currentBettingType == BetType.LINE) {
+                        closest = spot;
+                        minDistance = distance;
+                    }
+                }
+                for (BettingSpot spot : cornerSpots) {
+                    currentWager += spot.stack.getTotal();
+                    float distance = spot.distance(x, y);
+                    if (distance < minDistance && currentBettingType == BetType.CORNER) {
+                        closest = spot;
+                        minDistance = distance;
+                    }
+                }
+                if (minDistance > 50.0f) return; // has to be somewhat close to the point we're looking for
 
                 int before = closest.stack.getTotal();
                 int after = placeBet(closest.stack, leftSide.getBetAmount());
@@ -193,14 +230,75 @@ public class RouletteScreen extends TableScreen implements ActionCompletedListen
             }
         });
 
+        centerBetButton = new Button(assets.getSkin());
+        centerBetButton.setSize(100, 100);
+        centerBetButton.setPosition(260, stage.getHeight() - 110);
+        centerBetButton.setColor(menuButtonColor);
+        Image centerBetImage = new Image(assets.getTexture(Assets.TEX_NAME.ROULETTE_BET_CENTER));
+        centerBetButton.add(centerBetImage);
+        centerBetButton.pad(10);
+        centerBetButton.addListener(new ActorGestureListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                currentBettingType = BetType.ON;
+                centerBetButton.setColor(menuButtonColor);
+                lineBetButton.setColor(notSelectedColor);
+                cornerBetButton.setColor(notSelectedColor);
+            }
+        });
+
+        lineBetButton = new Button(assets.getSkin());
+        lineBetButton.setSize(100, 100);
+        lineBetButton.setPosition(260, stage.getHeight() - 210);
+        lineBetButton.setColor(notSelectedColor);
+        Image lineBetImage = new Image(assets.getTexture(Assets.TEX_NAME.ROULETTE_BET_LINE));
+        lineBetButton.add(lineBetImage);
+        lineBetButton.pad(10);
+        lineBetButton.addListener(new ActorGestureListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                currentBettingType = BetType.LINE;
+                centerBetButton.setColor(notSelectedColor);
+                lineBetButton.setColor(menuButtonColor);
+                cornerBetButton.setColor(notSelectedColor);
+            }
+        });
+
+        cornerBetButton = new Button(assets.getSkin());
+        cornerBetButton.setSize(100, 100);
+        cornerBetButton.setPosition(260, stage.getHeight() - 310);
+        cornerBetButton.setColor(notSelectedColor);
+        Image cornerBetImage = new Image(assets.getTexture(Assets.TEX_NAME.ROULETTE_BET_CORNER));
+        cornerBetButton.add(cornerBetImage);
+        cornerBetButton.pad(10);
+        cornerBetButton.addListener(new ActorGestureListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                currentBettingType = BetType.CORNER;
+                cornerBetButton.setColor(notSelectedColor);
+                lineBetButton.setColor(notSelectedColor);
+                cornerBetButton.setColor(menuButtonColor);
+            }
+        });
+
+
         // add the actors to the stage
         stage.addActor(tableImage);
         stage.addActor(wheel);
         stage.addActor(numberBoard);
         stage.addActor(spinButton);
         stage.addActor(undoBetButton);
+        stage.addActor(centerBetButton);
+        stage.addActor(lineBetButton);
+        stage.addActor(cornerBetButton);
 
-        for (BettingSpot spot : bettingSpots) {
+        for (BettingSpot spot : onSpots) {
+            stage.addActor(spot.stack);
+        }
+        for (BettingSpot spot : lineSpots) {
+            stage.addActor(spot.stack);
+        }
+        for (BettingSpot spot : cornerSpots) {
             stage.addActor(spot.stack);
         }
     }
@@ -208,7 +306,35 @@ public class RouletteScreen extends TableScreen implements ActionCompletedListen
     private void calculateWinnings(int number) {
         int won = 0;
         int initialWager = 0;
-        for (BettingSpot spot : bettingSpots) {
+        for (BettingSpot spot : onSpots) {
+            if(spot.stack.getTotal() > 0) {
+                initialWager += spot.stack.getTotal();
+                if(spot.winningNumbers.contains(number)) {
+                    won += spot.stack.getTotal() * spot.payout;
+                    addToBalance(won  + spot.stack.getTotal());
+                    spot.stack.popStack(true);
+                } else {
+                    won -= spot.stack.getTotal();
+                    spot.stack.popStack(false);
+                }
+                spot.stack.setTotal(0);
+            }
+        }
+        for (BettingSpot spot : lineSpots) {
+            if(spot.stack.getTotal() > 0) {
+                initialWager += spot.stack.getTotal();
+                if(spot.winningNumbers.contains(number)) {
+                    won += spot.stack.getTotal() * spot.payout;
+                    addToBalance(won  + spot.stack.getTotal());
+                    spot.stack.popStack(true);
+                } else {
+                    won -= spot.stack.getTotal();
+                    spot.stack.popStack(false);
+                }
+                spot.stack.setTotal(0);
+            }
+        }
+        for (BettingSpot spot : cornerSpots) {
             if(spot.stack.getTotal() > 0) {
                 initialWager += spot.stack.getTotal();
                 if(spot.winningNumbers.contains(number)) {
